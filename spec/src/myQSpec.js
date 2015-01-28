4c222d06e1333a49b70ab9b05a5bf8ff27bedd83
Matthew Nielsen <xunker@pyxidis.org>
2015-01-27 23:14:50 -0700

    Specs for myQ#authenticate.

--- spec/src/myQSpec.js
+++ spec/src/myQSpec.js
@@ -10,7 +10,7 @@ describe("myQ", function() {
     subject = new myQ();
   });
 
-  describe("#isAuthenticated()", function(){
+  describe("#isAuthenticated", function(){
     describe('#securityToken is defined', function() {
       beforeEach(function() {
         subject.securityToken = 'xxx';
@@ -27,7 +27,82 @@ describe("myQ", function() {
     });
   });
 
-  describe(".authenticateUrl()", function(){
+  describe("#authenticate", function(){
+    beforeEach(function() {
+      myQ.ajax = {};
+    });
+
+    var emailAddress = 'x@y.z';
+    var password = 'password';
+    var successCB = function() {};
+    var failureCB = function() {};
+
+    it('calls .ajax with options and callbacks', function() {
+      spyOn(myQ, 'ajax');
+      subject.authenticate(emailAddress, password, successCB, failureCB);
+      expect(myQ.ajax).toHaveBeenCalledWith(
+        { url: myQ.authenticateUrl(emailAddress, password), type: 'json' },
+        jasmine.any(Function), jasmine.any(Function)
+      );
+    });
+
+    describe("valid credentials", function() {
+      var securityToken = 'xxx';
+
+      beforeEach(function() {
+        myQ.ajax = function(_,success_cb,_) {
+          success_cb({ "SecurityToken":securityToken});
+        };
+      });
+
+      it('sets #securityToken on the instance', function() {
+        subject.authenticate(null, null, function(){}, null);
+        expect(subject.securityToken).toEqual("xxx");
+      });
+      it('executes the success callback', function() {
+        var observer = {callback: function(){}};
+        spyOn(observer, 'callback');
+        subject.authenticate(null, null, function() { observer.callback() }, null);
+        expect(observer.callback).toHaveBeenCalled();
+      });
+    });
+    describe("invalid credentials", function() {
+      beforeEach(function() {
+        subject.securityToken = 'xxx';
+        myQ.ajax = function(_,success_cb,_) { success_cb({}); };
+      });
+
+      it('clears #securityToken on the instance', function() {
+        subject.authenticate(null, null, null, function(){});
+        expect(subject.securityToken).toEqual(undefined);
+      });
+      it('executes the failure callback', function() {
+        var observer = {callback: function(){}};
+        spyOn(observer, 'callback');
+        subject.authenticate(null, null, null, function() { observer.callback() });
+        expect(observer.callback).toHaveBeenCalled();
+      });
+    });
+    describe("request error", function() {
+      beforeEach(function() {
+        subject.securityToken = 'xxx';
+        myQ.ajax = function(_,_,failure_cb) { failure_cb({}); };
+      });
+
+      it('clears #securityToken on the instance', function() {
+        subject.authenticate(null, null, null, null, function(){});
+        expect(subject.securityToken).toEqual(undefined);
+      });
+      it('executes the error callback', function() {
+        var observer = {callback: function(){}};
+        spyOn(observer, 'callback');
+        subject.authenticate(null, null, null, null, function() { observer.callback() });
+        expect(observer.callback).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe(".authenticateUrl", function(){
     var emailAddress = 'xxx@yyy.com';
     var password = 'p@ssw0rd';
 
@@ -45,7 +120,7 @@ describe("myQ", function() {
     });
   });
 
-  describe(".devicesUrl()", function(){
+  describe(".devicesUrl", function(){
     var token = 'xxx';
 
     it('return a constructed url', function() {
